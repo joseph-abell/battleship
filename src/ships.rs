a764eb539463d0487fa9e1768ea5d33fb2726263
Joseph Abell <jsphabll@gmail.com>
2023-02-26 21:46:28 +0000

    Update ships.rs to include a four ship and remove one ships

--- src/ships.rs
+++ src/ships.rs
@@ -1,33 +1,33 @@
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Ship {
-	OneBlock,
 	TwoBlock,
 	ThreeBlock,
+	FourBlock
 }
 
 #[derive(Debug)]
 pub struct ShipTracker {
-	one_block: usize,
 	two_block: usize,
 	three_block: usize,
+	four_block: usize,
 }
 
 impl ShipTracker {
-	pub fn new(one_block: usize, two_block: usize, three_block: usize) -> Self {
+	pub fn new(two_block: usize, three_block: usize, four_block: usize) -> Self {
 		Self {
-			one_block,
 			two_block,
 			three_block,
+			four_block,
 		}
 	}
 
 	pub fn get_next_unset_ship(&self) -> Option<Ship> {
-		if self.one_block > 0 {
-			Some(Ship::OneBlock)
-		} else if self.two_block > 0 {
+		if self.two_block > 0 {
 			Some(Ship::TwoBlock)
 		} else if self.three_block > 0 {
 			Some(Ship::ThreeBlock)
+		} else if self.four_block > 0 {
+			Some(Ship::FourBlock)
 		} else {
 			None
 		}
@@ -35,20 +35,20 @@ impl ShipTracker {
 
 	pub fn set_ship(&mut self, kind: &Ship) {
 		match kind {
-			Ship::OneBlock => {
-				self.one_block -= 1;
-			}
 			Ship::TwoBlock => {
 				self.two_block -= 1;
 			}
 			Ship::ThreeBlock => {
 				self.three_block -= 1;
 			}
+			Ship::FourBlock => {
+				self.four_block -= 1;
+			}
 		}
 	}
 
 	pub fn get_ships(&self) -> (usize, usize, usize) {
-		(self.one_block, self.two_block, self.three_block)
+		(self.two_block, self.three_block, self.four_block)
 	}
 }
 
@@ -58,32 +58,32 @@ fn keep_track_of_ships() {
 
 	assert_eq!(ships.get_ships(), (3, 2, 1));
 	let mut this_ship = ships.get_next_unset_ship().unwrap();
-	assert_eq!(this_ship, Ship::OneBlock);
+	assert_eq!(this_ship, Ship::TwoBlock);
 	ships.set_ship(&this_ship);
 
 	assert_eq!(ships.get_ships(), (2, 2, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
-	assert_eq!(this_ship, Ship::OneBlock);
+	assert_eq!(this_ship, Ship::TwoBlock);
 	ships.set_ship(&this_ship);
 
 	assert_eq!(ships.get_ships(), (1, 2, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
-	assert_eq!(this_ship, Ship::OneBlock);
+	assert_eq!(this_ship, Ship::TwoBlock);
 	ships.set_ship(&this_ship);
 
 	assert_eq!(ships.get_ships(), (0, 2, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
-	assert_eq!(this_ship, Ship::TwoBlock);
+	assert_eq!(this_ship, Ship::ThreeBlock);
 	ships.set_ship(&this_ship);
 
 	assert_eq!(ships.get_ships(), (0, 1, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
-	assert_eq!(this_ship, Ship::TwoBlock);
+	assert_eq!(this_ship, Ship::ThreeBlock);
 	ships.set_ship(&this_ship);
 
 	assert_eq!(ships.get_ships(), (0, 0, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
-	assert_eq!(this_ship, Ship::ThreeBlock);
+	assert_eq!(this_ship, Ship::FourBlock);
 	ships.set_ship(&this_ship);
 
 	assert_eq!(ships.get_ships(), (0, 0, 0));
